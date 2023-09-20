7b17e405308fc934e23c0e986a0dc80d240744f1
TypeError92 <simonwalgenbach@posteo.de>
2023-09-20 16:21:54 +0100

    add GET /api/leaderboard

--- app.py
+++ app.py
@@ -17,10 +17,12 @@ def api():
 @auth.admin
 def api_questions():
     return make_response(
-        {"questions": [
-            {"question": "IN ATTIC", "answer": "TITANIC", "category": "movie"},
-            {"question": "CORKY", "answer": "ROCKY", "category": "movie"},
-            {"question": "NOT PUT", "answer": "TOP GUN", "category": "movie"}]
+        {
+            "questions": [
+                {"question": "IN ATTIC", "answer": "TITANIC", "category": "movie"},
+                {"question": "CORKY", "answer": "ROCKY", "category": "movie"},
+                {"question": "NOT PUT", "answer": "TOP GUN", "category": "movie"}
+            ]
         },
         200)
 
@@ -35,11 +37,37 @@ def api_users():
 @auth.user
 def api_users__username(username):
     if request.method == 'GET':
-        return make_response({"user": {"username": username}}, 200)
+        return make_response(
+            {"user": {"username": username}},
+            200
+        )
     elif request.method == 'PATCH':
         pass
     elif request.method == 'DELETE':
         pass
+
+
+@app.route('/api/leaderboard', methods=['GET'])
+@auth.user
+def get_leaderboard():
+    return make_response(
+        {"entries": [
+            {"rank": 1, "username": "phil", "score": 1234},
+            {"rank": 2, "username": "charlotte", "score": 1111},
+            {"rank": 3, "username": "chris", "score": 999}
+        ]},
+        200
+    )
+
+
+@app.route('/api/leaderboard', methods=['PATCH'])
+@auth.admin
+# TODO: accept user/score pairs in request body
+def patch_leaderboard():
+    return make_response(
+        {},
+        200
+    )
 
 
 if __name__ == '__main__':
--- endpoints.py
+++ endpoints.py
@@ -1,29 +1,76 @@
 endpoints = {
-  "GET /api/": {
-    "description": "serves up a json representation of all available endpoints"
-  },
-  "GET /api/questions/random/": {
-    "description": "serves up a random question",
-    "queries": ["category_name", "number", "game_id"],
-    "example_response": [
-        {"question": "IN ATTIC", "answer": "TITANIC", "category": "movie"},
-        {"question": "CORKY", "answer": "ROCKY", "category": "movie"},
-        {"question": "NOT PUT", "answer": "TOP GUN", "category": "movie"}
-    ]
-  },
-  "POST /api/users": {
-    "description": "creates a new user and serves up a the new user object"
-  },
-  "GET /api/users/username": {
-    "description": "returns an existing user"
-  },
-  "PATCH /api/users/username": {
-    "description": "patches an existing user"
-  },
-  "DELETE /api/users/username": {
-    "description": "deletes a user"
-  }
 
+    # /API
 
+    "GET /api": {
+        "description": "serves up a json representation of all available endpoints",
+        "access": "any",
+        "queries": {}
+    },
 
+    # /API/LEADERBOARD
+
+    "GET /api/leaderboard": {
+        "access": "any",
+        "description": "serves up an array representation of the current leaderboard",
+        "queries": {"limit": {"description": "number of entries to be returned;"
+                                             "if omitted, the entire leaderboard will be returned",
+                              "type": "integer",
+                              "default": None},
+                    "page": {"description": "page of leaderboard to be returned;"
+                                            "will only be considered in combination with a 'limit' query",
+                             "type": "integer",
+                             "default": 1}},
+        "example_response": {"entries": [
+            {"rank": 1, "username": "phil", "score": 1234},
+            {"rank": 2, "username": "charlotte", "score": 1111},
+            {"rank": 3, "username": "chris", "score": 999}
+        ]}
+    },
+
+    "PATCH /api/leaderboard": {
+        "access": "admin",
+        "description": "submits player scores to update leaderbo",
+        "queries": {"limit": {"description": "number of entries to be returned;"
+                                             "if omitted, the entire leaderboard will be returned",
+                              "type": "integer",
+                              "default": None},
+                    "page": {"description": "page of leaderboard to be returned;"
+                                            "will only be considered in combination with a 'limit' query",
+                             "type": "integer",
+                             "default": 1}},
+        "example_response": {"entries": [
+                {"rank": 1, "username": "phil", "score": 1234},
+                {"rank": 2, "username": "charlotte", "score": 1111},
+                {"rank": 3, "username": "chris", "score": 999}
+            ]}
+    },
+    "GET /api/questions/": {
+        "description": "serves up a list of question/answer pairs",
+        "queries": ["category_name", "number", "game_id"],
+        "example_response": {
+            "questions": [
+                {"question": "IN ATTIC", "answer": "TITANIC", "category": "movie"},
+                {"question": "CORKY", "answer": "ROCKY", "category": "movie"},
+                {"question": "NOT PUT", "answer": "TOP GUN", "category": "movie"}
+            ]
+        }
+    },
+    "POST /api/users": {
+        "description": "creates a new user and serves up a the new user object",
+        "access": "admin",
+        "queries": {},
+        "example_request":
+    },
+
+    # /api/users/username
+    "GET /api/users/username": {
+        "description": "returns an existing user"
+    },
+    "PATCH /api/users/username": {
+        "description": "patches an existing user"
+    },
+    "DELETE /api/users/username": {
+        "description": "deletes a user"
+    }
 }
