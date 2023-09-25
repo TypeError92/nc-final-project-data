47eb107505e84224bc68ffa3d3b6432a11e9b28b
typeerror92 <simonwalgenbach@posteo.de>
2023-09-25 17:35:52 +0100

    add is_good_anagram util function

--- controllers/utils.py
+++ controllers/utils.py
@@ -3,3 +3,14 @@ def is_anagram(pair: tuple[str, str]):
     letters1 = sorted(list(s1.replace(' ', '').upper()))
     letters2 = sorted(list(s2.replace(' ', '').upper()))
     return letters1 == letters2
+
+
+def is_good_anagram(pair: tuple[str, str]):
+    if not is_anagram(pair):
+        return False
+    s1, s2 = pair
+    common_words = set(s1.split(' ')) & set(s2.split(' '))
+    for common_word in common_words:
+        if common_word not in ['A', 'I', 'THE']:
+            return False
+    return True
--- controllers/utils_test.py
+++ controllers/utils_test.py
@@ -1,5 +1,5 @@
-import pytest
-from utils import is_anagram
+from controllers.utils import is_anagram, is_good_anagram
+
 
 def test_is_anagram():
     # Returns a boolean
@@ -12,3 +12,14 @@ def test_is_anagram():
     assert is_anagram(('THE HOBBIT', 'BOTH BE HIT'))
     # Is case-insensitive
     assert is_anagram(('The Hobbit', 'BOTH BE HIT'))
+
+
+def test_is_good_anagram():
+    # Returns False for non-anagram
+    assert not is_good_anagram(('HE HIT BOB', 'THE HOBBIT'))
+    # Returns False if there are non-whitelisted common words
+    assert not is_good_anagram(('BOB CAN ACT', 'COB BAN ACT'))
+    # Returns true if there are no common words
+    assert is_good_anagram(('THE HOBBIT', 'BOTH BE HIT'))
+    # Allows whitelisted common words
+    assert is_good_anagram(('THE HOT BIB', 'THE HOBBIT'))
