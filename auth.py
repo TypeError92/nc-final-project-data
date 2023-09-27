277226382437a94b0d3a6fc133a19a7483d398c0
TypeError92 <132596140+TypeError92@users.noreply.github.com>
2023-09-27 10:46:48 +0100

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,25 +1,16 @@
+import os
 from flask import make_response, request
 from functools import wraps
 
+username, password = os.getenv('API_USERNAME'), os.getenv('API_PASSWORD')
 
-def user(f):
+
+def auth(f):
     @wraps(f)
     def decorated(*args, **kwargs):
         auth = request.authorization
         if (auth and
-                ((auth.username == 'admin' and auth.password == 'admin') or
-                 (auth.username == 'user' and auth.password == 'user'))):
-            return f(*args, **kwargs)
-        return make_response('You are not logged in!', 401, {'WWW-Authenticate': 'Basic realm="Login Required"'})
-
-    return decorated
-
-
-def admin(f):
-    @wraps(f)
-    def decorated(*args, **kwargs):
-        auth = request.authorization
-        if auth and auth.username == 'admin' and auth.password == 'admin':
+                ((auth.username, auth.password == username, password):
             return f(*args, **kwargs)
         return make_response('You are not logged in!', 401, {'WWW-Authenticate': 'Basic realm="Login Required"'})
 
