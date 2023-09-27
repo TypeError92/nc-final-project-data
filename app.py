cb2ab4b8a7e441283f276fbc3e17f7749cade6b8
TypeError92 <132596140+TypeError92@users.noreply.github.com>
2023-09-27 10:47:43 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import auth
+from auth import auth
 from controllers.dev_controllers import get_num_of_connections
 from controllers import get_questions, get_user, post_user
 from endpoints import endpoints
@@ -10,34 +10,37 @@ app = Flask(__name__)
 # TODO: Add all endpoints here.
 
 @app.route('/api')
-@auth.admin
+@auth
 def api():
     return endpoints
 
 
 @app.route('/api/questions')
+@auth
 def api_questions():
     return get_questions()
 
 
 @app.route('/api/stats/num-of-connections')
-@auth.admin
+@auth
 def stats_num_of_connections():
     return get_num_of_connections()
 
 
 @app.route('/api/users/sign-in', methods=['POST'])
+@auth
 def api_users_sign_in():
     return get_user(request)
 
 
 @app.route('/api/users/sign-up', methods=['POST'])
+@auth
 def api_users_sign_up():
     return post_user(request)
 
 
 @app.route('/api/leaderboard', methods=['GET'])
-@auth.user
+@auth
 def get_leaderboard():
     return make_response(
         {"entries": [
